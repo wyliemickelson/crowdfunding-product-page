fd43cccec2abebe009d3b0aa375e05673499997e
wyliemickelson <wyliecoyote910@gmail.com>
2022-07-13 11:10:50 -0700

    added modal functionality and continue popups on pledges

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
 import "../scss/main.scss";
+
+const backProjectBtn = document.getElementById('backProjectBtn');
+const selectionModal = document.getElementById('selectionModal');
+const selectionModalCloseBtn = document.getElementById('selectionModalCloseBtn');
+const pledgeRadioBtns = document.getElementsByClassName('pledge_radio');
+
+Array.from(pledgeRadioBtns).forEach((btn) => btn.addEventListener('change', () => {
+  pledgeRadioBtnHandler(btn);
+}))
+
+backProjectBtn.addEventListener('click', () => {
+  toggleHidden(selectionModal);
+});
+
+selectionModalCloseBtn.addEventListener('click', () => {
+  toggleHidden(selectionModal);
+});
+
+function toggleHidden(element) {
+  element.classList.toggle('hidden');
+}
+
+function pledgeRadioBtnHandler(checkedBtn) {
+  let uncheckedBtns = Array.from(document.querySelectorAll('input[name="pledge"]:not(:checked)'));
+  let pledgeConfirm = checkedBtn.parentElement.parentElement.nextElementSibling;
+  pledgeConfirm.classList.remove('hidden');
+  uncheckedBtns.forEach((uncheckedBtn) => {
+    pledgeConfirm = uncheckedBtn.parentElement.parentElement.nextElementSibling;
+    pledgeConfirm.classList.add('hidden');
+  })
+}
